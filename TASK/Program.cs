f127653ef13c0bc00b53b8b0915e03cf5837b6c8
TatianaBaranova <tatiana.barashkina@gmail.com>
2022-12-15 16:51:45 +0300

    добавлено решение задачи

---
+++ TASK/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array = {"Hello","2","world",":-)"};
+
+PrintArray(FormArrayWithLength(array));
+
+string[] FormArrayWithLength(string[] arrayInput)
+{
+    string[] resultArray = new string[arrayInput.Length];
+    for (int i = 0; i < arrayInput.Length; i++)
+    {
+        int length = arrayInput[i].Length;
+        if (length <= 3)
+        {
+            resultArray[i] = arrayInput[i];
+        }
+    }
+    return resultArray;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
